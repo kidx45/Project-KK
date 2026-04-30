3c96e1fd56dd3e6e43ed2ca3838bb7b3d796f5d5
kidx45 <kidustessema91@gmail.com>
2026-04-30 16:25:16 +0300

    Fix-And-Add

--- Backend-Team/db/test/main_test.go
+++ Backend-Team/db/test/main_test.go
@@ -14,11 +14,17 @@ import (
 var testQueries *sqlc.Queries
 var testDB *sql.DB
 func TestMain(m *testing.M) {
-	config, err := utils.LoadEnv("../../.env")
-	if err != nil {
-		log.Fatal("cannot load env:", err)
+	config := utils.Config{
+		DB_URL:         os.Getenv("DB_URL"),
+		PORT:           os.Getenv("PORT"),
+		DB_DRIVER_NAME: os.Getenv("DB_DRIVER_NAME"),
 	}
 
+	if config.DB_URL == "" || config.DB_DRIVER_NAME == "" {
+		log.Fatal("DB_URL or DB_DRIVER_NAME is not set in environment variables")
+	}
+
+	var err error
 	testDB, err = sql.Open(config.DB_DRIVER_NAME, config.DB_URL)
 
 	if err != nil {
