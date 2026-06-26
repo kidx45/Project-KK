e0ca727a7d5495e5b573a3543156e0ab32c69d88
kidx45 <kidustessema91@gmail.com>
2026-06-26 12:06:14 +0200

    File-Remove-And-Docker-Compose-Fix

--- utils/env.go
+++ utils/env.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"os"
 	"time"
 
 	"github.com/spf13/viper"
@@ -20,7 +21,16 @@ func LoadEnv(path string) (Config, error) {
     // Load the .env file from the current directory
 	err := viper.ReadInConfig()
 	if err != nil {
-		return Config{}, err
+		return Config{
+			DB_URL: os.Getenv("DB_URL"),
+			PORT: os.Getenv("PORT"),
+			DB_DRIVER_NAME: os.Getenv("DB_DRIVER_NAME"),
+			SYMMETRIC_SECRET_KEY: os.Getenv("SYMMETRIC_SECRET_KEY"),
+			ACCESS_TOKEN_DURATION: func() time.Duration {
+				d, _ := time.ParseDuration(os.Getenv("ACCESS_TOKEN_DURATION"))
+				return d
+			}(),
+		}, nil
 	}
 
 	AppConfig := Config{}
